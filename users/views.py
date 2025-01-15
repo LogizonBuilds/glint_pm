12da9df75bc37ac18678d498a7610d8ed82dfc2a
Ayobami Alaran <ayobamidele006@gmail.com>
2025-01-15 23:28:38 +0100

    Add root API view and update URL routing

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from sparky_utils.response import service_response
+from sparky_utils.advice import exception_advice
 
 # Create your views here.
+
+
+class RootAPIView(APIView):
+    """Root API View"""
+
+    def get(self, request, *args, **kwargs):
+        """Get method"""
+        return service_response(
+            message="Welcome to Glint PM API",
+            data={},
+            status_code=200,
+            status="success",
+        )
