304f32ac8d666180c9e152c123db2de386d3a747
Ayobami Alaran <ayobamidele006@gmail.com>
2025-01-16 18:14:43 +0100

    Add portfolio app with model and admin registration

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Portfolio
 
 # Register your models here.
+
+
+class PortfolioAdmin(admin.ModelAdmin):
+    list_display = ["name", "description"]
+    search_fields = ["name"]
+
+
+admin.site.register(Portfolio, PortfolioAdmin)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from sparky_utils.decorators import str_meta
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+
+@str_meta
+class Portfolio(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    image = CloudinaryField("image")
+    project_link = models.URLField(null=True, blank=True)
+    github_link = models.URLField(null=True, blank=True)
