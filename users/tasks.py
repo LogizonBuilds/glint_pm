57cf2d50f5f6473a93725bd1a039d40ca82e9994
Ayobami6 <ayobamidele006@gmail.com>
2025-03-23 15:05:03 +0100

    Add error handling to verify_payment_status_task and order transactions by date

--- users/tasks.py
+++ users/tasks.py
@@ -27,17 +27,26 @@ def send_email_verification_task(subject, message, username, user_email):
 @shared_task
 def verify_payment_status_task():
     # get all pending trancactions
-    transactions = Transaction.objects.filter(
-        transaction_status=TransactionStatus.PENDING.value
-    )
-    for transaction in transactions:
-        tx_ref = transaction.transaction_reference
-        flutter_sdk = FlutterSDK(tx_ref=tx_ref)
-        status = flutter_sdk.get_transaction_status()
-        print("This is the status: ", status)
-        if status == "successful":
-            transaction.transaction_status = TransactionStatus.SUCCESS.value
-            transaction.save()
-        elif status == "failed":
-            transaction.transaction_status = TransactionStatus.FAILED.value
-            transaction.save()
+    try:
+        transactions = Transaction.objects.filter(
+            transaction_status=TransactionStatus.PENDING.value
+        ).order_by("-transaction_date")
+        for transaction in transactions:
+            tx_ref = transaction.transaction_reference
+            flutter_sdk = FlutterSDK(tx_ref=tx_ref)
+            status = flutter_sdk.get_transaction_status()
+            print("This is the status: ", status)
+            if status == "successful":
+                transaction.transaction_status = TransactionStatus.SUCCESS.value
+                transaction.save()
+            elif status == "failed":
+                transaction.transaction_status = TransactionStatus.FAILED.value
+                transaction.save()
+    except Exception:
+        traceback.print_exc()
+        ErrorLog.objects.create(
+            app_name="verify_payment_status_task",
+            traceback=traceback.format_exc(),
+            severity="ERROR",
+        )
+        logger.error(traceback.format_exc())
